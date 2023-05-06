5bc764a4cf4ddf2ce34cc81a9f49adc64c93b855
Shuvam Halder <108192514+shuvam2722@users.noreply.github.com>
2023-05-06 19:07:38 +0530

    Update Frontpage.jsx

--- src/pages/Frontpage.jsx
+++ src/pages/Frontpage.jsx
@@ -49,7 +49,7 @@ function Frontpage() {
               <a href="#contact_page">
                 <button className="btn primary-btn">Hire Me</button>
               </a>
-              <a href="./shuvamhalder.pdf" download>
+              <a href="shuvamhalder.pdf" download>
                 <button className="btn highlighted-btn">Get Resume</button>
               </a>
             </div>
