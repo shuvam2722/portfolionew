a8790c058b174756901150ca741584c7f686ef2b
Shuvam Halder <108192514+shuvam2722@users.noreply.github.com>
2023-05-08 17:47:28 +0530

    Add files via upload

--- src/pages/components/Work.jsx
+++ src/pages/components/Work.jsx
@@ -10,7 +10,7 @@ function Works() {
         "Campus PlacementsOppurtunity through Superset.Hiring Consisted of 5 round in total.Online Quantitative Assessment,Coding Test,English Assessment,Technical Interview and followed by HR Interview.Click Below to get the detailed hiring workflow and interview questions asked.",
       image: "https://www.logo.wine/a/logo/Tech_Mahindra/Tech_Mahindra-Logo.wine.svg",
       github: "https://github.com",
-      demo: "https://demo.com",
+      demo: "https://lh3.googleusercontent.com/2bpnsWPZnQwqmlLWZqKU6KO_r3AWuPus33QuIJ6GcBZAK8fZlifxaEtHDxDdUvWGDaE0wSfaehcBBwPL0o_6gPTaJZRvTWMlstU0mwPXSuVfKkyEy7SECCiiQAE7FiahVQ95Zmetl2kcgp4Yn1k-EMFW0gWgAphltHt4ftgNIjRJJTUZ6Fj5Edxy2P082rjJWWwEfSRP2rUhTjtSW1Y7RhaRgKBPjkYHyAQIE95iS__awnQ7k4zaT-E7_fHEawmar16pxqhBwusCQGGMyz3tMXyWrWVmpjOhShGa3LgBeTUF_sEA-iKdAKEooVY-4ai5lS-i03bMijrZaDuDgG-av-hRP5KcbwHy_eGz8JbYOKly3VEPnHSlCiLK8czQZhWrVoK-vbYkxYiEC3E0sqkB_uufhDaBo_iLdfcvFkQrTyTwsP1OpXyf0a1_2OlLwEeZsioSlz7O1lKahBb43KyDpSeLBXL8iKNPIYV4Fa0MT4gW8c-BRGzM1BW9MtConFO1W3qZFaCtH9hWWer4By2JxBy40fZcUNuhhyJuCAUabbLTgMK5NIcQgB5yUY8sPP1sSfhfEBfK8D2_rb1d8u40UsG1QevSKJ8Z8GjAj-m5P3qjHQl8s6OHJ_S5v_EXZ-Gv_O577DVj28bp0PXNj9oMR2sHdytjVq5OG-HqXy3KvTTBtf2sjO2r9zhbBFSn3tfny2lKrtgIFRCt6DLOGzJH_Pmkqu5gmSoCY1Xn9ulebOjSKgJyMgGPTnYgYp8Rk2y2bxEnp6xsMbs-JqQa6ndAta5CCiTU6tk91beuZXr1WdIV0Q9CVFg78qFfu3TypbY_NqXNjVnReOR2G8UOHRLuuHKbz6plVJ6WO5_FSEnD-CMRZaJ0RIYYVICLdxUxOyPVPeOym0-v51vafK-S4-MQl4bJdKiCFwAJFfz3-NvNvFHm0e05MZn-mqWHuIGdMQdFR-IS7-tk3i5T2sDCwW43k40zV-YFT89qJuLZ1msCaJxf7atsG5inYHliodFALlpSOBLt3n_k2JE7W2MQbmy03EG1=w628-h817-s-no?authuser=3",
       company: "Tech Mahindra",
     },
     {
@@ -20,27 +20,27 @@ function Works() {
         "TCS Free NQT(Off Campus) for 2023 batch was conducted in the month of august,2022.Total Hiring process was done virtually.Numerical Ability,Verbal Ability,Reasoning Ability for Ninja Role and Advanced Aptitude,2 Coding Questions and these all for Digital Roles.Click Below to get interview questions",
       image: "https://s7ap1.scene7.com/is/content/TCSCOMprod/Logo",
       github: "https://github.com",
-      demo: "https://demo.com",
+      demo: "https://lh3.googleusercontent.com/3mpwNE4eUz9EiZ1B2iFEdQii3AEnU1kLB5aSiODzVC2kaobyiCLk98hlk10qrKnlZpO8P3Hju-CCReihROVJVBkMrjbQYYw0wGr7tRPKrY2aSjGPSZZMchtbmBbyeqgVfope7clnGkMxxuCiOU9GAqT3r3zhU1dl5LB5qijHDalVfJYwJObsa91xQJniqQ7tWA57lrjeJ-g4jRNDncz_kzYeWooQWaVTuFQ_UaENs56KQFLCsYnCvHxygctu9XhZX6z2f-MwghZRYdw36I5OPG1NChhOEQS-lSrHBFhfHe50F-rxmh6LwACX3a46B1wKfkjJFRbai8EpbKtG-c04Xjicq57uumlKFgq3bYI3D2F3GUeJoKoa8KT4WJzUqQPuJyHAE0Tj3hwLA68MmsZcdBMZxPXnKrZURcCqP7MyBWFGv3t5WTrzhNNwvkvFn5GpXREBOiJk5Nrlwve-urHJLaWkM6Lzo8lBvzgbjkDlyL2-Wz-RxcICymIS4Kk69zcaUWBG5Kh-MTtvGjIJxrSxENrF1fFnIkAdHp3m_3RRY-RcHJsByZbITYjOWQJV7tiCR7t6iGkH_ODlRxztqXavDuE3l6pg2RPaGEdKUpqpoMvLknOSlxR_hjZhf-zPo83bWdvistJPYelHLoabTbhfkY-eUV1-PzrtSuWxHgNygT4LpYFuPnsflxmu8AVIY72EZxb0TShZae-n52bUXlUfXZ5nqnov3aLkSvHjMPsek1SWs4RG8lu18eK0UYI8bGBdpkdzmNJzA6d2lcW9boFZloImMiBDVu0l8I923SgmOUiRMobdgraRG57EcI1N5mkpNuIURnqpVJMl8W2jtZF2YIOZnMGi_qSygWX8ow3GfzTGHhrs3rpMTi3wB11FpxTlGL_gTTL7o2LqaFnNp3MB1Fn6X_A6JSkrn7yB9RRLj5_9fJ-7F9a7Jl7841U6DjL0_QtBNZpaQhXT819TTas-YsfrfPbNbFEmiNlANu4OGF9oWqYlImV0E0Annx5IB1EPNIUQ4Eg4XZLhsrrJfSbILmjo=w572-h816-s-no?authuser=3",
       company: "Tata Consultancy Services",
     },
     {
       id: 3,
       title: "Software Engineer",
       description:
-        "Hiring Consisted of 2 major round,First round consisted of 5 quite hard level coding questions and followed by an hour based Technical and HR Interview on Frontend Technologies like ReactJS.Got Offer Letter in the month of August.Click below to get the coding questions.",
+        "Listed in career portal,the Hiring Consisted of 2 major round,First round consisted of 5 quite hard level coding questions and followed by an hour based Technical and HR Interview on Frontend Technologies like ReactJS.Got Offer Letter in the month of August.Click below to get the coding questions.",
       image: "https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_bfcc7d257cff47aae47fb42b7b48fb95/groundtruth-ads-manager.png",
       github: "https://github.com",
-      demo: "https://demo.com",
+      demo: "https://lh3.googleusercontent.com/wN9NksVploL0KAW2EEp2FKcs_0G4SYTBA_XYzVlpJXthduqpchpGGzWh4o5bF2U5R8ZX4Rtj10caITBQrbzlA-77Ct_itNQTHEHDcmedGuKzgZVZKMQv2vOvvBUPXkrVT6EpnMEVKQPJizNIki0kw93cfm8S4-xQSsRT29NMJLJa6Pr8ROKJzWIhXqUTbZA8lReg6iUIg_5r5ULmRiDVKeuFEreI0IvMqKH-G9fjZAjBnGDiZTYdmzWT2yeoJIp6OpmqrbQUbxISqrb57EnOCNqXpLZ_cRn3YQzNSYutxQnhVetzYOAh5moZ1C4oxyUdpy9hxhQQ7jnNtVJR3gGfvtkjNLKdAHmvy-OrDiXMNKo9Mr8rIvoVgM64vPy2RMUF6P0TMWcc1wIOAoJnMDufTPHeuEoBmcTNoqmnxCim65nn7rD8B-Jk_jXrkJbX5uSyF86dWu6IXgsWqOENAZO_m8ew08RatErpbkd6_6abmaegXiBq-xff3cLX9FA_VOnhrmFAIMPIyG23yC1lT-QWo32z-Q1tx1pHZuqzuab_QQZsQ5EvAP8i0xgtRMWfiMKtG7JYQUrEMnDdgLHQW4NTQb-zI4y9pxKY0-uiu1HR3NOFGC7It72WVdogjKbhVf2z4dn4WfouRKiz1LuRC5ZSiYIzzMnt7awnRzYhjLc9yMOi-r-63l-aSyDppFz12EZwAdjPC40cI-XQ3RLpFYL1HchF0NDc15VkKe3r0-BrNO5uza5uxxV_V8XAHusWAnceGm21Y8OIWyzGZbsZ2gw40nH7-ujGr5Lxn1RuItRDT8bY91AGt1H1S2RamzdFG-J7RcPdk6_HwWtZq7tE_L70qz-DDp0r7_QN4LWDqlh244ssgIvTvR4r6kW4uYn7RTk2V65QLxp1U8YpmupaXrx1UiTyLoioRYr42krWj5em0QpIPkcguTnCNVTLe0AKPrD8SIg27ygxUJU9mXgou6cBpQdvjBkr2LCw58XJewykQRALzZ7I0ND9drlh9zGFTx8fYNW6wNFPvPtG2CUvtHtwD6Ej=w476-h625-s-no?authuser=3",
       company: "GroundTruth",
     },
     {
       id: 4,
       title: "Graduate Engineer Trainee",
       description:
-        "Campus Placement Oppurtunity,managed by CIEM.Hiring Consisted of 2 Apti Rounds and TR and HR round.Though I could not make it to the company but it was one of the best experience.I am sharing my exlperience,click the link below to see.",
+        "Campus Placement Oppurtunity,managed by CIEM and sponsored by Superset.Hiring Consisted of 2 Apti Rounds and TR and HR round.Though I could not make it to the company but it was one of the best experience.I am sharing my exlperience,click the link below to see.",
       image: "https://www.logo.wine/a/logo/Hexaware_Technologies/Hexaware_Technologies-Logo.wine.svg",
       github: "https://github.com",
-      demo: "https://demo.com",
+      demo: "https://lh3.googleusercontent.com/-C3xJMULUT45VXxND9HFsnrKvC9dLRDrKE6UJc82VAR2bLRfN7T7nfPViUDr3r0teX5dZF6LzjdPsiKqwyAA0qUlhKcM3du2JWNUCJdJpu1tM1XrfC88QErqB92UrLfe2wyCQAN8oFEjhiskMXno4sHkvFPIidhEfmmVJ_3wqoPJEDn4a3vrfxlMyvli-R1zH-9CoTXvpKeOkqbNonbhhF2w92syvO_vKqvQRL5ZiKpRWyOBeTdax4FIp_T70yxLdc8wfHsJutbrj4ifClfEBc5156iMYRCyOO-EnQyz8fRf0s5CiLmQ3gH66hByBf2geRhI5wMuRYCQe-VQIizbd4qSPL9DF7kbNhmB4tSuFS2fiNhe2BL4By4RKEhaAKUxW3rA8_Z7qnrhzWAE1cvTMEyrx8c9tH_zVQkbnXa_hl4raempRWKbeo6Cy5he5XyDA4WtTt_I3ZLL73nhGwAkYC1SWni8WpI8RXpiIMVPLuc4dHbT0NCYhV8YoHn9Z8xuQI72XaRGu9fg8neGvZItwgDQ7_9Q3q2X6sLnzRnjl7YYHci8vj6oz1yTs-425u_vPC9HLgZ9r2nENY3lpjnzkjYO0UgZ9NtHxcOocVxF1FDU65cAP5W1KC9jq6hDxrnXJExxsoQDiU1yYCsHIw0toNT8nA6skeW8NIZS6KxOvh8t7iq3uBWpGSSc56D2SsKySHvSTd96wDgda10PQ89YnCjO63wKC9WUoM3EkkJTrtJckkfNxXkSia995ddMn0EUp_R5a60aU1nZrl5zECYbPT4AuxBBzGgBJEV0HNrJgIoqiXKsVroetrdrBSBl5KoOqK_5LNoZbLxP_cOmNBvRDJIxpBbsHXRaJ5poCxvXKu0CYjQbLLBN9wxbZ0vdyseWnumRDhCmQSOkSqZj4ECUUJWhn_ORRRfVKd5cb0MVPJx0occLTPPhmfXmLAXpr0t093CGQskJHytU--dI5pJsokj9jb83hsP30sTrlHH7u3bnKDkl78jaXp-bZYLu_0QjZ4WROFFOHAe1rscFl-F1nC8Q=w1476-h771-s-no?authuser=3",
       company: "Hexaware Technologies",
     },
   ];
@@ -48,18 +48,24 @@ function Works() {
   return (
     <div className='works-container'>
       <h2>My Interview Experiences<span>(As</span> a <span>Fresher)</span></h2>
-      <div className='works-wrapper'>
+      <div className='works-wrapper' >
         {works.map((work) => (
-          <div key={work.id} className='work-card slide-in'>
+          <div key={work.id} 
+          className='work-card slide-in' 
+          
+          work-card-aos="zoom-in-up"
+          work-card-aos-duration="2000">
+            
             <img src={work.image} alt={work.title} />
             <div className='company-name'>{work.company}</div>
+            
             <div className='work-info'>
               <h3>{work.title}</h3>
               <p>{work.description}</p>
               <div className='button-wrapper'>
                 <a className='button' href={work.github} target='_blank' rel='noreferrer'>
                   Exp.
-                </a><a className='button' href={work.demo} target='_blank' rel='noreferrer'>
+                </a><a className='button1' href={work.demo} target='_blank' rel='noreferrer'>
                   View Offer Letter
                 </a>
               </div>
