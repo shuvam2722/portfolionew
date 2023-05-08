466b8a8353a36c91860bfc6fbd115637a8e89d5f
Shuvam Halder <108192514+shuvam2722@users.noreply.github.com>
2023-05-08 19:37:01 +0530

    Update Works.jsx

--- src/pages/components/Works.jsx
+++ src/pages/components/Works.jsx
@@ -1,16 +1,1 @@
-import React from "react";
-import Work from "./components/Work";
 
-
-import "./Works.css";
-const EducationCert = () => {
-  return (
-    <>
-      <div className="educ_cert" id="my-background">
-        <Work/> 
-      </div>
-    </>
-  );
-};
-
-export default EducationCert;
