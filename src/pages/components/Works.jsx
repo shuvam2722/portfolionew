5bfb3a103315102a110963687f56534e2e018594
Shuvam Halder <108192514+shuvam2722@users.noreply.github.com>
2023-05-08 11:20:07 +0530

    Add files via upload

---
+++ src/pages/components/Works.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Work from "./components/Work";
+
+
+import "./Works.css";
+const EducationCert = () => {
+  return (
+    <>
+      <div className="educ_cert" id="my-background">
+        <Work/> 
+      </div>
+    </>
+  );
+};
+
+export default EducationCert;
