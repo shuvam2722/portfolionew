74a1bfb2849574a59ea7178b711d2b58e6bba152
Shuvam Halder <108192514+shuvam2722@users.noreply.github.com>
2023-05-10 00:12:59 +0530

    Add files via upload

--- src/pages/components/achieve.jsx
+++ src/pages/components/achieve.jsx
@@ -1,92 +1,69 @@
-import React from "react";
-import "./achieve.css";
-import dyp_college_img from "../../Image/college1.JPG";
-import college_logo from "../../Image/college_logo.png";
-function Education() {
-  const my_education_data = [
-    {
-      index: 1,
-      education_logo_link: college_logo,
-      college_name: "Calcutta Institute of Engineering and Management",
-      college_degree_name: "B.Tech in Information Technology",
-      education_spam: "2019-2023",
-      education_info: [
-        {
-          index: 1,
-          educ_info:
-            "⚡ I have studied basic software engineering subjects like Computer Networks, DBMS, Data Structure , OS etc.",
-        },
-        {
-          index: 2,
-          educ_info:
-            "⚡ Apart from this, I have done courses on Python, and Full Stack Development.",
-        },
-        {
-          index: 3,
-          educ_info:
-            "⚡  I have implemented several projects based on what I've learnt by my Self.",
-        },
-      ],
-      btn_name: "Visit College Website",
-      btn_link: "https://ciem.ac.in",
-    },
-  ];
+import React from 'react';
+import './achieve.css';
+
+const achievementsData = [
+  {
+    title: 'First Achievement',
+    description: 'This is the description of the first achievement. It can be multiple lines long and should describe the achievement in detail.',
+    date: 'May 2021',
+    link: 'https://example.com',
+    image: 'https://d299ydywi1tak7.cloudfront.net/media/colleges/50/media/campus.jpg'
+  },
+  {
+    title: 'Leading a College Cricket Team',
+    description: 'Been an active part of CIEM Cricket Team since 4 year.Lead from the front and took my to the Semi Finals and Finals of many Inter College Cricket Championship.',
+    date: 'Will definitely miss CIEM Ground :)',
+    link: 'https://m.facebook.com/photo.php?fbid=165203716262300&id=100083180186537&set=a.123630193752986&eav=Afb0LKiOr7U-jU5KGSRQT7EkHG1bVPW8NDzI9D7uW3XSEAqXt6CiI9ZCg-YWMGwZWF8&paipv=0&source=48&ref=content_filter',
+    image: 'https://i.ibb.co/2sFrNS8/IMG-7605.jpg'
+  },
+  {
+    title: 'Inter College Treasure Hunt Champions',
+    description: 'Participated and won treasure hunt competetion in CIEM Celsius 2023,Found and solve all the hints and clues within 20mins. :)',
+    date: 'If you know.. you know :)',
+    link: 'https://example.com',
+    image: 'https://i.ibb.co/80kCnFY/IMG-3617.jpg'
+  },
+  {
+    title: 'Fourth Achievement',
+    description: 'This is the description of the fourth achievement. It can be multiple lines long and should describe the achievement in detail.',
+    date: 'July 2021',
+    link: 'https://example.com',
+    image: 'https://d299ydywi1tak7.cloudfront.net/media/colleges/50/media/campus.jpg'
+  },
+];
+
+const AchievementCard = ({ title, description, date, link, image }) => {
   return (
-    <>
-      <div className="educ_container">
-      <div className="certificates_header_container">
-          <h2>Education</h2>
-        <div className="educ_div_section">
-          {my_education_data.map((data, index) => {
-            return (
-              <>
-                <div className="educ_upper_section" key={index}>
-                  <div className="educ_container_one">
-                    <img src={dyp_college_img} alt="educ_image" />
-                  </div>
-                  <div className="educ_container_two">
-                    <div className="upper_container_section">
-                      <div className="upper_left">
-                        <img
-                          src={data.education_logo_link}
-                          alt="college logo"
-                        />
-                      </div>
-                      <div className="upper_middle">
-                        <h3>{data.college_name}</h3>
-                        <div className="upper_right">
-                          <h4>{data.college_degree_name}</h4>
-                          <h5 style={{ color: "green" }}>
-                            {data.education_spam}
-                          </h5>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="educ_lower_section">
-                      {data.education_info.map((indata, index) => {
-                        return (
-                          <div className="educ_data_info" key={index}>
-                            <p>{indata.educ_info}</p>
-                          </div>
-                        );
-                      })}
-                      <div className="educ_btn">
-                        <a href={data.btn_link}>
-                          <button>{data.btn_name}</button>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </>
-            );
-          })}
-        </div>
-        </div>
+    <div className="achievement_card">
+      <img src={image} alt={title} />
+      <div className="title">{title}</div>
+      <div className="description">{description}</div>
+      <div className="date">{date}</div>
+      <div className="link">
+        <a href={link}>Learn More</a>
       </div>
-    </>
+    </div>
   );
-}
+};
 
+const Achievements = () => {
+  return (
+    <div className="achievements_container">
+      <h1 className="achievements_header">My Achievements</h1>
+      <div className="achievements_list">
+        {achievementsData.map((achievement) => (
+          <AchievementCard
+            key={achievement.title}
+            title={achievement.title}
+            description={achievement.description}
+            date={achievement.date}
+            link={achievement.link}
+            image={achievement.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Education;
+export default Achievements;
