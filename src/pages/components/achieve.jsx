404b3dbe2b2992181df5462ba82a2463a7d761f2
Shuvam Halder <108192514+shuvam2722@users.noreply.github.com>
2023-05-10 00:54:17 +0530

    Add files via upload

--- src/pages/components/achieve.jsx
+++ src/pages/components/achieve.jsx
@@ -3,32 +3,32 @@ import './achieve.css';
 
 const achievementsData = [
   {
-    title: 'First Achievement',
-    description: 'This is the description of the first achievement. It can be multiple lines long and should describe the achievement in detail.',
-    date: 'May 2021',
+    title: 'Despedida 2023',
+    description: 'Recently got the convocation ceremony and gifts from my beloved IT juniors of my college.The day was a blast with a mixed emotions going on in our heads as we are leaving now.',
+    date: 'Juniors,teachers,College Premises will always be unforgettable :(',
     link: 'https://example.com',
-    image: 'https://d299ydywi1tak7.cloudfront.net/media/colleges/50/media/campus.jpg'
+    image: 'https://i.ibb.co/5hC4hjP/IMG-20230510-003542.jpg'
   },
   {
-    title: 'Leading a College Cricket Team',
-    description: 'Been an active part of CIEM Cricket Team since 4 year.Lead from the front and took my to the Semi Finals and Finals of many Inter College Cricket Championship.',
+    title: 'Contribution to College Cricket Team',
+    description: 'Been an active part of CIEM Cricket Team since 4 year.Led from the front and took my team to the Semi Finals and Finals of many Inter College Cricket Championship.',
     date: 'Will definitely miss CIEM Ground :)',
     link: 'https://m.facebook.com/photo.php?fbid=165203716262300&id=100083180186537&set=a.123630193752986&eav=Afb0LKiOr7U-jU5KGSRQT7EkHG1bVPW8NDzI9D7uW3XSEAqXt6CiI9ZCg-YWMGwZWF8&paipv=0&source=48&ref=content_filter',
     image: 'https://i.ibb.co/2sFrNS8/IMG-7605.jpg'
   },
   {
     title: 'Inter College Treasure Hunt Champions',
-    description: 'Participated and won treasure hunt competetion in CIEM Celsius 2023,Found and solve all the hints and clues within 20mins. :)',
+    description: 'Participated and won treasure hunt competetion in CIEM Celsius 2023,Found and solved all the hints and clues and finished all the tasks within 20mins. :)',
     date: 'If you know.. you know :)',
     link: 'https://example.com',
     image: 'https://i.ibb.co/80kCnFY/IMG-3617.jpg'
   },
   {
-    title: 'Fourth Achievement',
-    description: 'This is the description of the fourth achievement. It can be multiple lines long and should describe the achievement in detail.',
+    title: 'Co Ordinated CIEM Sports Fest',
+    description: 'Been a Co Ordinator in CIEM Sports Fest, Celsius 2023.Organized various kinds of sports like Cricket,Football,Badminton,Table Tennis etc.',
     date: 'July 2021',
     link: 'https://example.com',
-    image: 'https://d299ydywi1tak7.cloudfront.net/media/colleges/50/media/campus.jpg'
+    image: 'https://i.ibb.co/xmLQ8p1/IMG-3001.jpg'
   },
 ];
 
@@ -49,7 +49,7 @@ const AchievementCard = ({ title, description, date, link, image }) => {
 const Achievements = () => {
   return (
     <div className="achievements_container">
-      <h1 className="achievements_header">My Achievements</h1>
+      <h1 className="achievements_header">Activities <span>and</span> Blogs</h1>
       <div className="achievements_list">
         {achievementsData.map((achievement) => (
           <AchievementCard
